cee0fe4b0027776ae9d89d284e47bc655712cb49
baltov3 <116825337+baltov3@users.noreply.github.com>
2025-09-11 17:36:16 +0300

    fixed lemonade section

--- JS/modules/modal.js
+++ JS/modules/modal.js
@@ -14,6 +14,8 @@ export const products = {
     price: "1.50лв",
     
   },
+
+  // Protein Shakes
   proteinWithRaspberry: {
     title: "Бял шоколад с малина",
     description: "Балансирано съчетание от еспресо, мляко и фина млечна пяна.",
@@ -48,6 +50,70 @@ export const products = {
     volume: "390ml",
     advice: "Комбинирайте с мляко за още по-добър вкус.(+0.50лв)",
     allergens: ["assets/imgs/milk.png"]
+  },
+// Lemonades
+mangoLemonade: {
+    title: "Лимонада Манго",
+    description: "Освежаваща лимонада със сироп от манго и цитрусови нотки.",
+    price: "2.50лв",
+    volume: "390ml",
+     advice: "Опитайте с резен лимон за допълнителна свежест.",
+    reminder: "Съдържа сироп от манго.",
+    allergens: []
+  },
+  elderLemonade: {
+    title: "Лимонада Бъз",
+    description: "Ароматна лимонада със сироп от бъз и свеж лимонов сок.",
+    price: "2.50лв",
+    volume: "390ml",
+    advice: "Опитайте с резен лимон за допълнителна свежест.",
+    reminder: "Съдържа сироп от бъз.",
+    allergens: []
+  },
+  forestFruitLemonade: {
+    title: "Лимонада Горски плодове",
+    description: "Плодов микс лимонада със сироп от горски плодове.",
+    price: "2.50лв",
+    volume: "390ml",
+    advice: "Комбинирайте с лед и мента.",
+    reminder: "Съдържа сироп от горски плодове.",
+    allergens: []
+  },
+  raspberryLemonade: {
+    title: "Лимонада Малина",
+    description: "Освежаваща лимонада със сироп от малина и лимонов сок.",
+    price: "2.50лв",
+    volume: "390ml",
+    advice: "Добавете лед за максимална свежест.",
+    reminder: "Съдържа сироп от малина.",
+    allergens: []
+  },
+  strawberryLemonade: {
+    title: "Лимонада Ягода",
+    description: "Лимонада със сироп от ягоди и лимон, подходяща за малки и големи.",
+    price: "2.50лв",
+    volume: "390ml",
+     advice: "Опитайте с резен лимон за допълнителна свежест.",
+    reminder: "Съдържа сироп от ягоди.",
+    allergens: []
+  },
+  orangeLemonade: {
+    title: "Лимонада Портокал",
+    description: "Класическа лимонада със сироп от портокал и свеж лимон.",
+    price: "2.50лв",
+    volume: "390ml",
+    advice: "Опитайте с резен лимон за допълнителна свежест.",
+    reminder: "Съдържа сироп от портокал.",
+    allergens: []
+  },
+  cherryLemonade: {
+    title: "Лимонада Вишна",
+    description: "Лимонада със сироп от вишна и лимон, с балансиран сладко-кисел вкус.",
+    price: "2.50лв",
+    volume: "390ml",
+     advice: "Опитайте с резен лимон за допълнителна свежест.",
+    reminder: "Съдържа сироп от вишна.",
+    allergens: []
   },
   // Добави още продукти тук
 };
