b31df0f4befefefab181bbc47a6b47239597ca8b
baltov3 <116825337+baltov3@users.noreply.github.com>
2025-09-11 16:50:52 +0300

    fixed section for proteins

--- JS/modules/modal.js
+++ JS/modules/modal.js
@@ -14,5 +14,42 @@ export const products = {
     price: "1.50лв",
     
   },
+  proteinWithRaspberry: {
+    title: "Бял шоколад с малина",
+    description: "Балансирано съчетание от еспресо, мляко и фина млечна пяна.",
+    price: "2.50лв",
+    volume: "390ml",
+    advice: "Комбинирайте с мляко за още по-добър вкус.(+0.50лв)",
+    reminder: "Може да добавите сметана по избор.",
+    allergens: ["assets/imgs/milk.png"]
+  },
+   peanutButter: {
+    title: "Фъстъчено масло",
+    description: "Кремообразна напитка с наситен вкус на фъстъчено масло и мляко.",
+    price: "2.50лв",
+    volume: "390ml",
+    advice: "Комбинирайте с мляко за още по-добър вкус.(+0.50лв)",
+    reminder: "Може да добавите сметана по избор.",
+    allergens: ["assets/imgs/milk.png"]
+  },
+  saltedCaramel: {
+    title: "Солен карамел",
+    description: "Сладко-солена напитка с карамелен сироп и щипка морска сол.",
+    price: "2.50лв",
+    volume: "390ml",
+    advice: "Комбинирайте с мляко за още по-добър вкус.(+0.50лв)",
+    reminder: "Може да добавите сметана по избор.",
+    allergens: ["assets/imgs/milk.png"]
+  },
+  nutella: {
+    title: "Шоколад с лешник (Нутела)",
+    description: "Наситена напитка с шоколад и лешников крем Нутела.",
+    price: "3.50лв",
+    volume: "390ml",
+    advice: "Комбинирайте с мляко за още по-добър вкус.(+0.50лв)",
+    allergens: ["assets/imgs/milk.png"]
+  },
   // Добави още продукти тук
 };
+
+ 
